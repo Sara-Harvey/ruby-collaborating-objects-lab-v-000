ef1f4e262e849e793949f19c26b3a77ca9184d73
Sara Harvey <saraharveynyc@gmail.com>
2019-02-06 19:53:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,20 +1,17 @@
 class MP3Importer
+  attr_reader :path, :files
 
-#parses the filenames in the spec/fixtures folder and sends them to Song to create a “library of music with artists that are unique.”
+  def initialize(path)
+    @path = path
+    @files = Dir.entries(path).grep(/.*\.mp3/)
+  end
 
-def initialize 
-  #path attribute that sets on initialization
-  MP3Importer.new('./spec/fixtures').import
+  def import
+    @files.each {|file| Song.new_by_filename(file)}
+  end
 end
 
-def files 
-  #google how get a list of files in a directory. Get only .mp3 files.
-  Dir["./spec/fixtures/mp3s"]
-end
-
-def import 
-  #Send filenames to Song by calling this code in the #import method: 
-  Song.new_by_filename(some_filename).
-  #This sends us to Song, specifically Song.new_by_filename.
-end
+  def print_songs
+    self.songs.each {|song| puts song.name} 
+   end
 end
