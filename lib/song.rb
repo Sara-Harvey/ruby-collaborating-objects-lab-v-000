5eefacdf2f7371bf3be8a1236d49014c50e2ce07
Sara Harvey <saraharveynyc@gmail.com>
2019-02-06 19:54:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,14 @@
-class Song 
-  
-  #Song creates songs for each filename and sends the artist's name (a string) to Artist.
-#Artist either creates the artist (if it doesn't exist yet) or finds the instance of the artist.
+class Song
+  attr_accessor :name, :artist
 
-  def self.new_by_filename(file)
-   constructor = self.new 
-   constructor.artist_name = file.split("-")[0]
-   constructor.name = file.split("-")[1]
-   constructor.genre = file.split("-")[2]
-end
+  def initialize(name)
+    @name = name
+  end
 
-  
-  
-  #Parse a filename to find the song name and artist name (every file separates the song and artist with " - ")
-#Create a new song instance using the string from the filename.
-#Associate the new song with an artist using helper method: Song#artist().
-#Return the new song instance.
-
-Song#artist_name=(name)
-#Collaborate with Artist.
-
-#Turn the artist's name as a string into an artist object.
-#Get the instance of the Artist class that represents that artist:
-#Either create that artist instance of find the artist instance.
-#Send the artist's name (a string from the parsed filename) to Artist with Artist.find_or_create_by_name(artists-name-here).
-
-#Assign the song to the artist (because an artist has many songs)
-#When we have the artist instance, call on Artist#add_song(some_song).
-
+  def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
 end
